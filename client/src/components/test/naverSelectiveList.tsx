ba0642e992d51f117035d1f67d965399d06ec4f5
46gom <vito0920!>
2019-05-14 17:02:24 +0900

    add test component & controller

--- client/src/components/test/naverSelectiveList.tsx
+++ client/src/components/test/naverSelectiveList.tsx
@@ -4,26 +4,52 @@ import { Card } from 'antd'
 
 const { Meta } = Card
 
-const NaverSelective: React.FC<any> = ({}) => {
-  const selectiveData = getSelectiveDatas()
-  const listData = selectiveData.list
+interface OwnState {
+  selectiveData: Object
+}
 
-  return (
-    <div>
-      {listData.map((data, index) => {
-        if (!data.representImagePathForWest) return
-        return (
-          <Card
-            hoverable
-            style={{ width: '90%', marginBottom: '10px' }}
-            cover={<img alt={data.urlId} src={data.representImagePathForWest} />}
-          >
-            <Meta title={data.urlId} description="NAVER Selective Item" />
-          </Card>
-        )
-      })}
-    </div>
-  )
+class NaverSelective extends React.Component<any, OwnState> {
+  constructor(props) {
+    super(props)
+    this.state = { selectiveData: {} }
+  }
+
+  async componentDidMount() {
+    await this.setData()
+  }
+
+  async componentDidUpdate() {
+    await this.setData()
+  }
+
+  setData = async () => {
+    const selectiveData = await getSelectiveDatas()
+    if (selectiveData) {
+      this.setState({ selectiveData })
+    }
+  }
+
+  render() {
+    const listData = this.state.selectiveData && this.state.selectiveData['list']
+    return (
+      <div>
+        {listData &&
+          listData.map((data, index) => {
+            if (!data.representImagePathForWest) return
+            return (
+              <Card
+                hoverable
+                style={{ width: '90%', marginBottom: '10px' }}
+                cover={<img alt={data.urlId} src={data.representImagePathForWest} />}
+                key={`selective_data_${index}`}
+              >
+                <Meta title={data.urlId} description="NAVER Selective Item" />
+              </Card>
+            )
+          })}
+      </div>
+    )
+  }
 }
 
 export default NaverSelective
