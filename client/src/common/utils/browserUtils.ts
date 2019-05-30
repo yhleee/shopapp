d6518a438788ca472376ca3d33c9c0dade391d5a
46gom <vito0920!>
2019-05-30 10:15:09 +0900

    add browser utils

---
+++ client/src/common/utils/browserUtils.ts
@@ -0,0 +1,49 @@
+export interface DisplayInfo {
+  clientWidth: number
+  clientHeight: number
+  scrollWidth: number
+  scrollHeight: number
+}
+
+export const getDisplaySize = () => {
+  if (!window || !document) return null
+  const documentElement = document.documentElement
+  const documentBody = document.getElementsByTagName('body')[0]
+
+  const x = window.innerWidth || documentElement.clientWidth || documentBody.clientWidth
+  const y = window.innerHeight || documentElement.clientWidth || documentBody.clientHeight
+  const w = documentElement.scrollWidth
+  const h = documentElement.scrollHeight
+
+  return {
+    clientWidth: Number(x),
+    clientHeight: Number(y),
+    scrollWidth: Number(w),
+    scrollHeight: Number(h),
+  }
+}
+
+export const getCookies = () => {
+  if (!document) return
+  const cookieStrings = document.cookie.split('; ')
+  const cookies = new Object()
+  cookieStrings.forEach(cookieString => {
+    try {
+      const cookieArray = cookieString.split('=')
+      cookies[cookieArray[0]] = cookieArray[1]
+    } catch (e) {}
+  })
+  return cookies
+}
+
+export const requestIFrame = (url: string) => {
+  const iframe = document.createElement('iframe')
+
+  iframe.style.display = 'none'
+  iframe.src = url
+
+  document.body.appendChild(iframe)
+  setTimeout(() => {
+    document.body.removeChild(iframe)
+  }, 1000)
+}
